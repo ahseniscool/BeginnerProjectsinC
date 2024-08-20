5351d8f0bdf9bf281de3f591d9c424b1a569e1f6
Ahsen Jauhar <122107859+ahseniscool@users.noreply.github.com>
2024-08-21 00:28:41 +1000

    Update TicTacToe.c
    swapper p2 for computer

--- TicTacToe.c
+++ TicTacToe.c
@@ -3,126 +3,114 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
-/* player 1 = X
-    player 2 = O
-*/
-
-/*****************************************************/
 
 void Printboard(int[3][3], int[2]);
-int Checkwin(int[3][3],int[2]);
-void Turninput (int[3][3],int[2]);
-// turns contains the turncount ( turns[0]) and the plyer whose current turn it is  ( turns[1])
-// board is simply the state of the board at the moment 
+int Checkwin(int[3][3], int[2]);
+void Turninput(int[3][3], int[2]);
+void ComputerTurn(int[3][3], int[2]);
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     int result = 0;
 
-    int turns[2] = {1,1};
+    int turns[2] = {1, 1};
     int board[3][3] = {
-        {0,0,0},
-        {0,0,0},
-        {0,0,0}
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
     };
 
-   do {
+    srand(time(NULL)); // Seed random number generator
+
+    do {
         printf("---------------------\n");
         printf("Turn Number %d\n", turns[0]);
         printf("---------------------\n\n");
-        Printboard(board,turns);
-        Turninput(board,turns);
-        result = Checkwin(board,turns);
-        if ( turns[1] == 1 ) {
+        Printboard(board, turns);
+
+        if (turns[1] == 1) {
+            Turninput(board, turns);
+        } else {
+            ComputerTurn(board, turns);
+        }
+
+        result = Checkwin(board, turns);
+        if (turns[1] == 1) {
             turns[1] = 2;
-        } else { turns[1] = 1; 
+        } else {
+            turns[1] = 1;
         }
         turns[0]++;
-   }  while (result == 0);
-   
-   result = Checkwin(board,turns);
+    } while (result == 0);
 
-   if ( result == 1 ) {
-    printf("\n3 in A ROW !!, Player 1 won\n");
+    result = Checkwin(board, turns);
 
-   } else if ( result == 2 ) {
-    printf("\n3 in A ROW !!, Player 2 won\n");
+    if (result == 1) {
+        printf("\n3 in A ROW !!, Player 1 won\n");
+    } else if (result == 2) {
+        printf("\n3 in A ROW !!, Player 2 (Computer) won\n");
+    } else {
+        printf("\nA tie ...\n");
+    }
 
-   } else {
-    printf("\nA tie ...\n");
-   }
-
-   printf("\n\n");
-   Printboard(board,turns);
-   exit(0);
+    printf("\n\n");
+    Printboard(board, turns);
+    exit(0);
 }
 
-/*****************************************************/
-// prints the baord after a turn //
-
-void 
-Printboard ( int board[3][3], int turn[2] ) 
-{
-    for ( int i = 0; i < 3 ; i++ ) {
-        for ( int j = 0; j < 3; j++ ) {
-            if( ( board[j][2-i] ) == 1 ) {
+void Printboard(int board[3][3], int turn[2]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[j][2 - i] == 1) {
                 printf("X");
-                } else if ( ( board[j][2-i] ) == 2 ) {
-                    printf("0");
-                } else {
-                    printf(" ");
-                }
-             if ( j!= 2 ) {
-                printf("|");
-            } 
+            } else if (board[j][2 - i] == 2) {
+                printf("O");
+            } else {
+                printf(" ");
             }
-        if ( i != 2 ) {
+            if (j != 2) {
+                printf("|");
+            }
+        }
+        if (i != 2) {
             printf("\n-----\n");
-        }         
-        }        
-        printf("\n");
-}
-
-/*****************************************************/
-// returns 0 if the game is still going, 1 if player 1 wins, 2 if player 2 wins , 3 for a tie//
-// checks 
-int 
-Checkwin(int board[3][3],int turns[2]) {
-for ( int i = 0; i < 3; i ++ ) {
-    if ( board[0][i] != 0 && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
-        return board[0][i];
+        }
     }
+    printf("\n");
 }
 
-for ( int i = 0; i < 3; i ++ ) {
-    if ( board[i][0] != 0 && board[i][0] == board[i][1] && board[i][0] == board[i][2]){
-        return board[i][0];
+int Checkwin(int board[3][3], int turns[2]) {
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] != 0 && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+            return board[0][i];
+        }
     }
-}
 
-if ( board[0][0] != 0 && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
-    return board[0][0];
-}
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+            return board[i][0];
+        }
+    }
 
-if ( board[0][2] != 0 && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
-    return board[0][2];
-}
+    if (board[0][0] != 0 && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+        return board[0][0];
+    }
 
-if ( turns[0] == 9 ) {
-    return 3;
-}
-return 0;
-} 
+    if (board[0][2] != 0 && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+        return board[0][2];
+    }
 
-/*****************************************************/
+    if (turns[0] == 9) {
+        return 3;
+    }
+    return 0;
+}
 
 void Turninput(int board[3][3], int turns[2]) {
     int x = 0, y = 0, satisfied = 0, attempts = 0;
-    char exit;
 
     while (attempts < 5) {
         printf("\n\nIt is Player %d's turn\nEnter the coordinate (x,y): ", turns[1]);
-        
+
         if (scanf(" (%d,%d)", &x, &y) == 2) {
             x--;
             y--;
@@ -136,7 +124,7 @@ void Turninput(int board[3][3], int turns[2]) {
         } else {
             printf("Invalid input format. Please enter coordinates in the format (x,y).\n");
         }
-        
+
         while (getchar() != '\n'); // Clear input buffer
         attempts++;
     }
@@ -146,5 +134,53 @@ void Turninput(int board[3][3], int turns[2]) {
         turns[1] = (turns[1] == 1) ? 2 : 1; // Switch player
     }
 }
-/***************************************************/
- 
+
+void ComputerTurn(int board[3][3], int turns[2]) {
+    // Check for a winning move
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == 0) {
+                board[i][j] = 2;
+                if (Checkwin(board, turns) == 2) {
+                    printf("Computer placed an O at (%d,%d)\n", i + 1, j + 1);
+                    return;
+                }
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    // Check if the player can win next move, and block it
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == 0) {
+                board[i][j] = 1;
+                if (Checkwin(board, turns) == 1) {
+                    board[i][j] = 2;
+                    printf("Computer blocked at (%d,%d)\n", i + 1, j + 1);
+                    return;
+                }
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    // Place randomly if no immediate threat or win is found
+    int empty_spots[9][2];
+    int empty_count = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == 0) {
+                empty_spots[empty_count][0] = i;
+                empty_spots[empty_count][1] = j;
+                empty_count++;
+            }
+        }
+    }
+
+    if (empty_count > 0) {
+        int rand_index = rand() % empty_count;
+        board[empty_spots[rand_index][0]][empty_spots[rand_index][1]] = 2;
+        printf("Computer placed an O at (%d,%d)\n", empty_spots[rand_index][0] + 1, empty_spots[rand_index][1] + 1);
+    }
+}
